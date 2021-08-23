09d4b85f50001fed1e9fb1785a68577f8345d2d1
Haram Jeong <niceharam@gmail.com>
2021-08-23 22:03:46 +0900

    Chap01

--- src/chap01/Hello.java
+++ src/chap01/Hello.java
@@ -1,7 +1,12 @@
 package chap01;
-
+/*
+ * 작성자: 정하람
+ * 작성일: 2021.08.23
+ */
 public class Hello {
+	//프로그램 실행 진입점
 	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
 		System.out.println("Hello, welcome to the java world!");
-	}
-}
+	} //end of main
+} //end of class
